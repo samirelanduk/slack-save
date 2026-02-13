0335704d0a481dbfb4f5f6f2f27fd1f34c5d52af
Sam Ireland <mail@samireland.com>
2026-02-13 02:30:04 +0000

    Get replies

--- slack.py
+++ slack.py
@@ -13,7 +13,7 @@ def main():
 
     # Save a list of all the users in the workspace
     output = {"people": {}, "conversations": {}}
-    #output["people"] = get_users(data)
+    output["people"] = get_users(data)
     save_output(output, output_path)
 
     for channel_id, channel_name in data["channels"].items():
@@ -45,16 +45,23 @@ def get_users(data):
     return users
 
 
-def get_all_messages(channel_id, data):
+def get_all_messages(channel_id, data, reply_ts=None):
     page = 1
     last_message_ts = None
     messages = []
     while True:
-        new_messages = get_messages_page(channel_id, data, latest_ts=last_message_ts)
+        new_messages = get_messages_page(channel_id, data, latest_ts=last_message_ts, reply_ts=reply_ts)
         if not new_messages: break
         if new_messages[-1]["ts"] == last_message_ts: break
-        log(f"Page {page}", indent=1)
+        if reply_ts and page == 1:
+            log(f"Getting replies to message {reply_ts}", indent=2)
+        elif reply_ts and page > 1:
+            log(f"Getting replies to message {reply_ts}, page {page}", indent=2)
+        else:
+            log(f"Page {page}", indent=1)
         new_messages = [message for message in new_messages if message.get("ts") != last_message_ts]
+        for message in new_messages:
+            check_replies(message, channel_id, data)
         messages += new_messages
         messages.sort(key=lambda x: x["ts"])
         page += 1
@@ -62,12 +69,22 @@ def get_all_messages(channel_id, data):
     return messages
 
 
-def get_messages_page(channel_id, data, latest_ts=None):
+def get_messages_page(channel_id, data, latest_ts=None, reply_ts=None):
+    path = "conversations.replies" if reply_ts else "conversations.history"
     params = {"channel": channel_id}
     if latest_ts: params["latest"] = latest_ts
-    return slack_post("conversations.history", data, params=params)["messages"]
-    
+    if reply_ts: params["ts"] = reply_ts
+    messages = slack_post(path, data, params=params)["messages"]
+    if reply_ts: messages.pop(0)
+    return messages
 
+
+def check_replies(message, channel_id, data):
+    if message.get("reply_count", 0) > 0:
+        message["replies"] = get_all_messages(channel_id, reply_ts=message["ts"], data=data)
+    else:
+        message["replies"] = []
+    
 
 def slack_request(method, url, data, params=None, indent=1):
     global sleep_time
